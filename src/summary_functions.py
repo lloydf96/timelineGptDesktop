2ec8dca40103e3042bf972cff1dc15f44df1ddf7
Praveen Kumar Murugaiah <59926314+praveen-kumar-data-science@users.noreply.github.com>
2023-07-10 13:44:55 -0500

    changed chunk overlap to 50

--- src/summary_functions.py
+++ src/summary_functions.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 ENCODING_NAME = "gpt-3.5-turbo"
 CHUNK_SIZE = 2000
-CHUNK_OVERLAP = 500
+CHUNK_OVERLAP = 50
 BULLET_SIGN = '->'
 API_KEY = st.secrets['chatgpt_api']
 openai.api_key = API_KEY
